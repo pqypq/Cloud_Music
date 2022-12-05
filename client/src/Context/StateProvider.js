ad47bf7ed029428e51b0a84c2a17773101491f24
Xinhui Yuan <pipipengn@gmail.com>
2022-12-04 23:59:28 -0800

    commit

--- client/src/Context/StateProvider.js
+++ client/src/Context/StateProvider.js
@@ -1,12 +1,12 @@
-import React, { useContext, useReducer } from "react";
+import React, {useContext, useReducer} from "react";
 
-const { Provider, Consumer} = React.createContext()
+const {Provider, Consumer} = React.createContext()
 export const StateContext = React.createContext();
 
-export const StateProvider = ({ reducer, initialState, children }) => (
-  <StateContext.Provider value={useReducer(reducer, initialState)}>
-    {children}
-  </StateContext.Provider>
+export const StateProvider = ({reducer, initialState, children}) => (
+    <StateContext.Provider value={useReducer(reducer, initialState)}>
+        {children}
+    </StateContext.Provider>
 );
 
 export const useStateValue = () => useContext(StateContext);
